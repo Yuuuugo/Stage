b36c9e94319d92123099049de3ee022f8a5cac1c
Yuuuugo <hugogabrielidis@gmail.com>
2022-03-22 13:33:53 +0100

    Add launch to JS dataset

---
+++ Data_JS/DataSet1/launch.py
@@ -0,0 +1,18 @@
+import threading
+import os
+import grpc
+from Data import nb_client
+
+if os.environ.get('https_proxy'):
+ del os.environ['https_proxy']
+if os.environ.get('http_proxy'):
+ del os.environ['http_proxy']
+
+semaphore = threading.Semaphore(nb_client)
+
+def run_command(cmd):
+    with semaphore:
+        os.system(cmd)
+
+for i in range(nb_client):
+    threading.Thread(target=run_command, args=("python3 modified_client.py --partition=" +str(i), )).start()
