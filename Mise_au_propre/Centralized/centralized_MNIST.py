c8dde9541575a76144597ed0d3b735ff55f7b46a
Yuuuugo <hugogabrielidis@gmail.com>
2022-05-19 14:16:24 +0200

    Slicing on MNIST done -> Do the curve of centralized now

--- Mise_au_propre/Centralized/centralized_MNIST.py
+++ Mise_au_propre/Centralized/centralized_MNIST.py
@@ -2,13 +2,13 @@ def run_centralized_MNIST(epochs, nbr_clients, directory_name):
     import pickle
     from Model.model_MNIST import create_model_MNIST
     from data.data_MNIST.Preprocessing_MNIST import X_train, X_test, y_train, y_test
+    import tensorflow as tf
 
-    print(len(X_train))
     model = create_model_MNIST()
-
+    # print(len(X_train) / 32)
     X_train_epochs = [[] for w in range(epochs)]
     y_train_epochs = [[] for w in range(epochs)]
-
+    all_history = {}
     for i in range(nbr_clients):
         X_train_i = X_train[
             int((i / nbr_clients) * len(X_train)) : int(
@@ -32,38 +32,29 @@ def run_centralized_MNIST(epochs, nbr_clients, directory_name):
                     ((actual_rnd + 1) / epochs) * len(y_train_i)
                 )
             ]
+            print(actual_rnd)
+            X_train_epochs[actual_rnd].append(X_train_i_actual_rnd)
+            y_train_epochs[actual_rnd].append(y_train_i_actual_rnd)
+    # print(len(X_train_epochs[0][0]))
+    # print(len(X_train_epochs[1][0]))
 
-            X_train_epochs[i].append(X_train_i_actual_rnd)
-            y_train_epochs[i].append(y_train_i_actual_rnd)
-
-    print(len(X_train_epochs[0][0]))
-    print(len(X_train_epochs[1][0]))
-
-    print("SIZEE = " + str(len(X_train_epochs[0])))
+    # print("SIZEE = " + str(len(X_train_epochs[0])))
     for i in range(epochs):
-        print(i)
         X_t = X_train_epochs[i][0]
         y_t = y_train_epochs[i][0]
+        # print("Size of X_train_epochs :" + str(len(X_train_epochs[i])))
+        # print(type(X_t))
+        for j in range(1, len(X_train_epochs[i])):
 
-        for j in range(1, len(X_train_epochs[epochs - 1])):
-            X_t = X_t + X_train_epochs[i][j]
-            y_t = y_t + y_train_epochs[i][j]
-        print(y_t)
+            X_t = tf.concat([X_t, X_train_epochs[i][j]], 0)
+            y_t = tf.concat([y_t, y_train_epochs[i][j]], 0)
+
         history = model.fit(
-            X_t,
-            y_t,
-            epochs=1,
-            validation_data=(X_test, y_test),
-            batch_size=32,
+            X_t, y_t, epochs=1, validation_data=(X_test, y_test), batch_size=32
         )
+        all_history["history_epochs_" + str(epochs)] = history
 
-    """ history = model.fit(
-        X_train,
-        y_train,
-        epochs=epochs,
-        validation_data=(X_test, y_test),
-        batch_size=32,
-    ) 
+    """
     list = []
     dict = history.history
     for key in dict.keys():
