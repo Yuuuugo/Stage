dfe4e84d88a8d88ff81b7835e403f4fb3add2231
Yuuuugo <hugogabrielidis@gmail.com>
2022-05-24 18:04:33 +0200

    do a change for the first duration of server

--- Mise_au_propre/results/load_result.py
+++ Mise_au_propre/results/load_result.py
@@ -5,31 +5,52 @@ import matplotlib.pyplot as plt
 import os
 
 
-dict = {}
+dict = {"server": {}, "centralized": {}}
 
-experience_path = "MNIST_FedAvg_clients_2_rounds2_20220524170429"
+experience_path = "MNIST_FedAvg_clients_2_rounds5_20220524180104"
 for root, dirs, files in os.walk(experience_path + "/", topdown=False):
     print(files)
     for filename in files:
-        print(filename)
-        unpickleFile = open(experience_path + "/" + filename, "rb")
-        new_dict = pickle.load(unpickleFile, encoding="latin1")
-        dict[filename] = new_dict
 
-dict["server"].pop(0)
+        if filename == "server":
+            unpickleFile = open(experience_path + "/" + filename, "rb")
+            server_metrics = pickle.load(unpickleFile, encoding="latin1")
+            server_duration = pickle.load(unpickleFile, encoding="latin1")
+            server_metrics.pop(
+                0
+            )  # We skip the evaluation round that happend before training
+            dict["server"]["metrics"] = server_metrics
+            dict["server"]["duration"] = server_duration
+
+        elif filename == "centralized":
+            unpickleFile = open(experience_path + "/" + filename, "rb")
+            centralized_metrics = pickle.load(unpickleFile, encoding="latin1")
+            centralized_duration = pickle.load(unpickleFile, encoding="latin1")
+
+            dict["centralized"]["metrics"] = centralized_metrics
+            dict["centralized"]["duration"] = centralized_duration
+
+        elif "client_" in filename:
+            unpickleFile = open(experience_path + "/" + filename, "rb")
+            new_dict = pickle.load(unpickleFile, encoding="latin1")
+            dict[filename] = {}
+            dict[filename]["metrics"] = new_dict
+            dict[filename]["duration"] = dict["server"]["duration"]
+
+
+print(dict)
 
 
 fig, ax = plt.subplots()
 
-rounds = [i for i in range(len(dict[list(dict.keys())[0]]))]
 
-
-for i in list(dict.keys()):
-    list = dict[i]
+for component in list(dict.keys()):
+    metrics = dict[component]["metrics"]
+    duration = dict[component]["duration"]
     y = []
-    for elements in list:
-        y.append(elements[1])
-    plt.plot(rounds, y, label=i)
+    for element in metrics:
+        y.append(element[1])
+    plt.plot(duration, y, label=component)
     plt.legend()
 
 fig.savefig(experience_path + "/metric_with_all_client.jpg")
